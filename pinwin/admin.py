dec003ae072997a592977e8af2ad1f797dcf8e88
kanroot <61951793+kanroot@users.noreply.github.com>
2021-06-05 19:13:31 -0400

    Cambio del almacenamineto del banner de local a la base de datos (#1)
    * migrations
    
    * imagenes guardadas en la base de datos

--- pinwin/admin.py
+++ pinwin/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import CreatorUser, CreatorImage
+
+admin.site.register(CreatorUser)
+admin.site.register(CreatorImage)
--- pinwin/models.py
+++ pinwin/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+
+class CreatorUser(AbstractUser):
+    username = models.CharField(verbose_name='Nombre de usuario', max_length=25, primary_key=True)
+    email = models.EmailField(verbose_name='Correo electronico', unique=True)
+    bio = models.TextField(verbose_name='Bio', max_length=280, blank=True, default='Perrito')
+    pin = models.IntegerField(verbose_name='Pins', blank=True, default=0)
+    banner = models.ImageField(upload_to='pinwin.ImageFile/bytes/filename/mimetype', blank=True, null=True)
+
+
+class CreatorImage(models.Model):
+    image = models.ImageField(null=True)
+    title = models.CharField(max_length=50, blank=True, default='Perrito')
+    descr = models.TextField(blank=True, default='Perrito')
+    user = models.ForeignKey(CreatorUser, on_delete=models.CASCADE)
+
+
+class ImageFile(models.Model):
+    bytes = models.TextField()
+    filename = models.CharField(max_length=255)
+    mimetype = models.CharField(max_length=50)
